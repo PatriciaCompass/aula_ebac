8165996ed79346bc88bdc21bb4572cbe9ae9fdeb
PatriciaCompass <patricia.lemos@compasso.com.br>
2024-05-13 20:22:08 -0300

    primeira automacao

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('pati@teste.com')
+        cy.get('#password').type('Senha@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, pati (não é pati? Sair)')
+    });
+
+});
