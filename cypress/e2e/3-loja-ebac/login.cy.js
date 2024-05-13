036a2d04416cb69f128ff77671359c4f732fe958
PatriciaCompass <patricia.lemos@compasso.com.br>
2024-05-13 20:43:19 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,13 +2,26 @@
 
 describe('Funcionalidade: Login', () => {
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    // afterEach(() => {
+    //     cy.screenshot()
+    // });
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('pati@teste.com')
         cy.get('#password').type('Senha@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, pati (não é pati? Sair)')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('pati.lemos@teste.com')
+        cy.get('#password').type('Senha@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     });
 
 });
