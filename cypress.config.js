b8a6b7cf2d4625f52cd80306b92bd3124409d768
PatriciaCompass <patricia.lemos@compasso.com.br>
2024-05-21 12:23:30 -0300

    mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -7,11 +7,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://lojaebac.ebaconline.art.br/",
-    video: true,
-    reporter: 'junit',
-    reporterOptions: {
-      mochaFile: 'results/my-test-output.xml',
-      toConsole: true,
+    video: true
     },
-  },
 });
